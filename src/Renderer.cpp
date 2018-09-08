17d92752b5bd57e722640bc66640aa681f06e02c
dimi309 <dimi309@users.noreply.github.com>
2018-09-08 03:25:35 +0200

    Setting OpenGL context version to 4.1.

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -344,7 +344,7 @@ namespace small3d {
     }
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
