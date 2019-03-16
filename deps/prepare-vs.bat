8eaa068b3ccc656d71450520ae2152841d690062
dimi309 <dimi309@users.noreply.github.com>
2019-03-16 16:27:22 +0100

    Corrected gtest compilation for Debug builds.

--- deps/prepare-vs.bat
+++ deps/prepare-vs.bat
@@ -81,8 +81,7 @@ cmake .. -G"%VSCONFIG%" -DBUILD_GMOCK=OFF -Dgtest_disable_pthreads=ON
 cmake --build . --config %BUILDTYPE%
 if %errorlevel% neq 0 exit /b %errorlevel%
 xcopy ..\googletest\include\gtest ..\..\include\gtest /i /s
-copy googletest\%BUILDTYPE%\gtest.lib ..\..\lib
-copy googletest\%BUILDTYPE%\gtest_main.lib ..\..\lib\
+if %BUILDTYPE%==Debug (copy googletest\%BUILDTYPE%\gtestd.lib ..\..\lib\gtest.lib && copy googletest\%BUILDTYPE%\gtest_maind.lib ..\..\lib\\gtest_main.lib) else (copy googletest\%BUILDTYPE%\gtest.lib ..\..\lib && copy googletest\%BUILDTYPE%\gtest_main.lib ..\..\lib\)
 for /r %%a in (*.pdb) do @copy /y "%%a" ..\..\bin
 cd ..\..
 rmdir /Q /S googletest-release-1.8.1
