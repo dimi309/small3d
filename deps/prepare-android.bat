63cb652c18bc00b4a664eda87bc13faf247435b6
dimi309 <dimi309@users.noreply.github.com>
2019-06-14 02:47:10 +0200

    Preparing libraries for android.

---
+++ deps/prepare-android.bat
@@ -0,0 +1,90 @@
+set CMAKE_DEFINITIONS=-DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_PLATFORM=android-26
+mkdir include
+mkdir lib
+
+7z x glm-0.9.9.0.zip
+if %errorlevel% neq 0 exit /b %errorlevel% 
+xcopy glm\glm include\glm /i /s
+rmdir /Q /S glm
+
+7z x libpng-1.6.34.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x libpng-1.6.34.tar
+cd libpng-1.6.34
+mkdir build
+cd build
+cmake .. -G"MinGW Makefiles" -DPNG_SHARED=OFF -DPNG_STATIC=ON -DPNG_TESTS=OFF %CMAKE_DEFINITIONS%
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy ..\*.h ..\..\include
+copy pnglibconf.h ..\..\include
+copy libpng.a ..\..\lib
+cd ..\..\
+rmdir /Q /S libpng-1.6.34
+del libpng-1.6.34.tar
+del pax_global_header
+
+7z x ogg-1.3.3.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x ogg-1.3.3.tar
+cd ogg-1.3.3
+mkdir build
+cd build
+cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF %CMAKE_DEFINITIONS%
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy ..\include\ogg ..\..\include\ogg /i /s
+copy include\ogg\config_types.h ..\..\include\ogg
+copy libogg.a ..\..\lib
+cd ..\..\
+rmdir /Q /S ogg-1.3.3
+del ogg-1.3.3.tar
+del pax_global_header
+
+set depspath=%cd%
+
+7z x vorbis-1.3.6.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x vorbis-1.3.6.tar
+cd vorbis-1.3.6
+mkdir build
+cd build
+cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF -DOGG_ROOT=%depspath% %CMAKE_DEFINITIONS%
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy ..\include\vorbis ..\..\include\vorbis /i /s
+copy lib\*.a ..\..\lib
+cd ..\..\
+rmdir /Q /S vorbis-1.3.6
+del vorbis-1.3.6.tar
+del pax_global_header
+
+7z x pa_stable_v190600_20161030.tgz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x pa_stable_v190600_20161030.tar
+cd portaudio
+mkdir build1
+cd build1
+cmake .. -G"MinGW Makefiles" -DPA_USE_WDMKS=OFF %CMAKE_DEFINITIONS%
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+copy ..\include\* ..\..\include
+copy libportaudio_static.a ..\..\lib
+cd ..\..\
+rmdir /Q /S portaudio
+del pa_stable_v190600_20161030.tar
+
+7z x freetype-2.9.1.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x freetype-2.9.1.tar
+cd freetype-2.9.1
+mkdir build
+cd build
+cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF %CMAKE_DEFINITIONS%
+cmake --build .
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy ..\include ..\..\include /s /e
+copy libfreetype.a ..\..\lib
+cd ..\..
+rmdir /Q /S freetype-2.9.1
+del freetype-2.9.1.tar
