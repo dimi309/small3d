52d3cb7b83e5242447df14549f00654dd91f89ee
dimi309 <dimi309@users.noreply.github.com>
2019-09-21 12:07:53 +0200

    Adding ios preparation script.

---
+++ deps/prepare-ios.sh
@@ -0,0 +1,81 @@
+export CMAKE_DEFINITIONS=-DCMAKE_BUILD_TYPE=Release
+-DCMAKE_TOOLCHAIN_FILE=../../../../ios.toolchain.cmake -DPLATFORM=OS64COMBINED 
+
+mkdir include
+mkdir lib
+
+unzip glm-0.9.9.0.zip
+cp -rf glm/glm include/
+rm -rf glm
+
+tar xvf zlib-1.2.11.tar.gz
+cd zlib-1.2.11
+mkdir build
+cd build
+cmake .. $CMAKE_DEFINITIONS
+cmake --build .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp ../zlib.h ../../include/
+cp zconf.h ../../include/
+cp libz.a ../../lib/
+cd ../../
+rm -rf zlib-1.2.11
+
+tar xvf libpng-1.6.34.tar.gz
+cd libpng-1.6.34
+mkdir build
+cd build
+cmake .. -DPNG_SHARED=OFF -DPNG_STATIC=ON -DPNG_TESTS=OFF -DZLIB_LIBRARY=$(pwd)/../../lib/libza -DZLIB_INCLUDE_DIR=$(pwd)/../../include $CMAKE_DEFINITIONS
+cmake --build .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp ../*.h ../../include/
+cp pnglibconf.h ../../include/
+cp libpng.a ../../lib/
+cd ../../
+rm -rf libpng-1.6.34
+
+tar xvf ogg-1.3.3.tar.gz
+cd ogg-1.3.3
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS=OFF $CMAKE_DEFINITIONS
+cmake --build .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp -rf ../include/ogg ../../include/
+cp include/ogg/config_types.h ../../include/ogg/
+cp libogg.a ../../lib/
+cd ../../
+rm -rf ogg-1.3.3
+
+tar xvf vorbis-1.3.6.tar.gz
+cd vorbis-1.3.6
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=$(pwd)/../../ $CMAKE_DEFINITIONS
+cmake --build .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp -rf ../include/vorbis ../../include/
+cp lib/*.a ../../lib/
+cd ../../
+rm -rf vorbis-1.3.6
+
+tar xvf bzip2-1.0.6.tar.gz
+cd bzip2-1.0.6
+make
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp bzlib.h ../include/
+cp libbz2.a ../lib/
+cd ..
+rm -rf bzip2-1.0.6
+
+tar xvf freetype-2.9.1.tar.gz
+cd freetype-2.9.1
+mkdir build
+cd build
+cmake .. -DBUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=$(pwd)/../../ $CMAKE_DEFINITIONS
+cmake --build .
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp -rf ../include/* ../../include/
+cp libfreetype.a ../../lib/
+cd ../..
+rm -rf freetype-2.9.1
