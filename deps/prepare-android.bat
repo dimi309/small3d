db29db642bd76cc2182b960424b26f038702182a
dimi309 <dimi309@users.noreply.github.com>
2020-05-24 15:17:56 +0200

    Corrected android deps preparation script error reporting.

--- deps/prepare-android.bat
+++ deps/prepare-android.bat
@@ -1,14 +1,15 @@
 REM For this to work, set the %NDK% variable to your ndk path. It should look like
 REM C:\Users\me\AppData\Local\Android\Sdk\ndk\20.0.5594570 for example. Also, the
 REM script needs to run in an environment with MinGW set up and no settings for
-REM Visual Studio. Finally, the %depspath% environment variable has to be set
-REM to point to the deps directory inside the small3d project.
+REM Visual Studio.
 
 mkdir include
 mkdir lib
 
+SET depspath=%cd%
+
 7z x glm-0.9.9.0.zip
-if %errorlevel% neq 0 exit /b %errorlevel% 
+if errorlevel 1 exit /B 
 xcopy glm\glm include\glm /i /s /y
 rmdir /Q /S glm
 
@@ -19,14 +20,14 @@ set CMAKE_DEFINITIONS=-DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain
 mkdir lib\%%A
 
 7z x libpng-1.6.37.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 7z x libpng-1.6.37.tar
 cd libpng-1.6.37
 mkdir build
 cd build
 cmake .. -G"MinGW Makefiles" -DPNG_SHARED=OFF -DPNG_STATIC=ON -DPNG_TESTS=OFF  %CMAKE_DEFINITIONS%
 cmake --build .
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 copy ..\*.h ..\..\include /y
 copy pnglibconf.h ..\..\include /y
 copy libpng.a ..\..\lib\%%A
@@ -36,14 +37,14 @@ del libpng-1.6.37.tar
 del pax_global_header
 
 7z x ogg-1.3.3.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 7z x ogg-1.3.3.tar
 cd ogg-1.3.3
 mkdir build
 cd build
 cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF %CMAKE_DEFINITIONS%
 cmake --build .
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 xcopy ..\include\ogg ..\..\include\ogg /i /s /y
 copy include\ogg\config_types.h ..\..\include\ogg /y
 copy libogg.a ..\..\lib\%%A
@@ -52,17 +53,15 @@ rmdir /Q /S ogg-1.3.3
 del ogg-1.3.3.tar
 del pax_global_header
 
-set depspath=%cd%
-
 7z x vorbis-1.3.6.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 7z x vorbis-1.3.6.tar
 cd vorbis-1.3.6
 mkdir build
 cd build
 cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF -DOGG_INCLUDE_DIRS=%depspath%/include -DOGG_LIBRARIES=%depspath%/lib/%%A/libogg.a %CMAKE_DEFINITIONS%
 cmake --build .
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 xcopy ..\include\vorbis ..\..\include\vorbis /i /s /y
 copy lib\*.a ..\..\lib\%%A
 cd ..\..\
@@ -71,14 +70,14 @@ del vorbis-1.3.6.tar
 del pax_global_header
 
 7z x freetype-2.9.1.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 7z x freetype-2.9.1.tar
 cd freetype-2.9.1
 mkdir build
 cd build
 cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF %CMAKE_DEFINITIONS%
 cmake --build .
-if %errorlevel% neq 0 exit /b %errorlevel%
+if errorlevel 1 exit /B
 xcopy ..\include ..\..\include /s /e /y
 copy libfreetype.a ..\..\lib\%%A
 cd ..\..
