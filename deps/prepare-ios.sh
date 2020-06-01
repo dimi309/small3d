9b8e65ca26fbc2daff3b10d7b77d50aceea22e1d
dimi309 <dimi309@users.noreply.github.com>
2020-06-01 18:24:50 +0200

    Merged ios dependency preparation scripts.

--- deps/prepare-ios.sh
+++ deps/prepare-ios.sh
@@ -1,8 +1,3 @@
-export CMAKE_DEFINITIONS="-GXcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64"
-
-# -DPLATFORM=OS64 and no -DARCHS for arm64
-# -DPLATFORM=OS -DARCHS=armv7 for armv7
-
 mkdir include
 mkdir lib
 
@@ -10,7 +5,65 @@ unzip glm-0.9.9.0.zip
 cp -rf glm/glm include/
 rm -rf glm
 
-./prepare-zlib-ios.sh
+export ARCH=arm64 # armv7 or arm64
+export CHOST=aarch64-apple-darwin* # arm-apple-darwin* or aarch64-apple-darwin*
+export SDK=iphoneos
+
+export SDKVERSION=9 #$(xcrun --sdk $SDK --show-sdk-version) # current version
+export SDKROOT=$(xcrun --sdk $SDK --show-sdk-path) # current version
+export PREFIX="/opt/$SDK-$SDKVERSION/$ARCH"
+
+export CC=$(xcrun --sdk $SDK --find gcc)" -fembed-bitcode"
+export CPP=$(xcrun --sdk $SDK --find gcc)" -E"
+export CXX=$(xcrun --sdk $SDK --find g++)
+export LD=$(xcrun --sdk $SDK --find ld)
+
+export CFLAGS="$CFLAGS -arch $ARCH -isysroot $SDKROOT -I$PREFIX/include -miphoneos-version-min=$SDKVERSION"
+export CPPFLAGS="$CPPFLAGS -arch $ARCH -isysroot $SDKROOT -I$PREFIX/include -miphoneos-version-min=$SDKVERSION"
+export CXXFLAGS="$CXXFLAGS -arch $ARCH -isysroot $SDKROOT -I$PREFIX/include"
+export LDFLAGS="$LDFLAGS -arch $ARCH -isysroot $SDKROOT -L$PREFIX/lib"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH":"$SDKROOT/usr/lib/pkgconfig":"$PREFIX/lib/pkgconfig"
+
+tar xvf zlib-1.2.11-noexample.tar.gz
+cd zlib-1.2.11
+./configure
+make
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp zlib.h ../include/
+cp zconf.h ../include/
+cp libz.a ../lib/
+cd ../
+rm -rf zlib-1.2.11
+
+tar xvf bzip2-1.0.8-use-env.tar.gz
+cd bzip2-1.0.8
+make bzip2
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+cp bzlib.h ../include/
+cp libbz2.a ../lib/
+cd ..
+rm -rf bzip2-1.0.8
+
+unset ARCH
+unset CHOST
+unset SDK
+unset SDKVERSION
+unset SDKROOT
+unset PREFIX
+unset CC
+unset CPP
+unset CXX
+unset LD
+unset CFLAGS
+unset CPPFLAGS
+unset CXXFLAGS
+unset LDFLAGS
+unset PKG_CONFIG_PATH
+
+export CMAKE_DEFINITIONS="-GXcode -DCMAKE_TOOLCHAIN_FILE=../../ios-cmake/ios.toolchain.cmake -DPLATFORM=OS64"
+
+# -DPLATFORM=OS64 and no -DARCHS for arm64
+# -DPLATFORM=OS -DARCHS=armv7 for armv7
 
 tar xvf libpng-1.6.37.tar.gz
 cd libpng-1.6.37
