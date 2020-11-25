15dd59ae338ed8bf69fcb3e6cdfaf64a6879d297
dimi309 <dimi309@users.noreply.github.com>
2020-11-25 06:19:00 +0100

    Android dependencies build script corrected.

--- deps/prepare-android.bat
+++ deps/prepare-android.bat
@@ -4,7 +4,7 @@ REM For this to work, set the %NDK% variable to your ndk path. It should look li
 REM C:\Users\user\AppData\Local\Android\Sdk\ndk\22.0.6917172 for example. Also, the
 REM script needs to run in an environment with MinGW set up and no settings for
 REM Visual Studio.
-
+setlocal enabledelayedexpansion
 set args_ok=false
 
 if /I "%~1" == "Debug" set args_ok=true
@@ -27,7 +27,7 @@ SET sourcepath=%cd%
 SET platformstr=android-28
 
 7z x glm-0.9.9.0.zip
-if errorlevel 1 exit /B 
+if "%errorlevel%" neq "0" endlocal & exit /b %errorlevel% 
 xcopy glm\glm include\glm /i /s /y
 rmdir /Q /S glm
 
@@ -36,7 +36,7 @@ for %%A in (x86,x86_64,armeabi-v7a,arm64-v8a) do (
 mkdir lib\%%A
 
 7z x libpng-1.6.37.tar.gz
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel%
 7z x libpng-1.6.37.tar
 cd libpng-1.6.37
 mkdir build
@@ -45,17 +45,19 @@ cmake .. -G"MinGW Makefiles" -DPNG_SHARED=OFF -DPNG_STATIC=ON -DPNG_TESTS=OFF^
  -DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_PLATFORM=%platformstr% -DANDROID_ABI=%%A^
  %CMAKE_DEFINITIONS%
 cmake --build .
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 copy ..\*.h ..\..\include /y
 copy pnglibconf.h ..\..\include /y
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 copy libpng.a ..\..\lib\%%A
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 cd ..\..\
 rmdir /Q /S libpng-1.6.37
 del libpng-1.6.37.tar
 del pax_global_header
 
 7z x ogg-1.3.3.tar.gz
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 7z x ogg-1.3.3.tar
 cd ogg-1.3.3
 mkdir build
@@ -64,17 +66,20 @@ cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF^
  -DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_PLATFORM=%platformstr% -DANDROID_ABI=%%A^
  %CMAKE_DEFINITIONS%
 cmake --build .
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 xcopy ..\include\ogg ..\..\include\ogg /i /s /y
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 copy include\ogg\config_types.h ..\..\include\ogg /y
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 copy libogg.a ..\..\lib\%%A
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 cd ..\..\
 rmdir /Q /S ogg-1.3.3
 del ogg-1.3.3.tar
 del pax_global_header
 
 7z x vorbis-1.3.6.tar.gz
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 7z x vorbis-1.3.6.tar
 cd vorbis-1.3.6
 mkdir build
@@ -84,16 +89,18 @@ cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF^
  -DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_PLATFORM=%platformstr% -DANDROID_ABI=%%A^
  %CMAKE_DEFINITIONS%
 cmake --build .
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 xcopy ..\include\vorbis ..\..\include\vorbis /i /s /y
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 copy lib\*.a ..\..\lib\%%A
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 cd ..\..\
 rmdir /Q /S vorbis-1.3.6
 del vorbis-1.3.6.tar
 del pax_global_header
 
 7z x freetype-2.9.1.tar.gz
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 7z x freetype-2.9.1.tar
 cd freetype-2.9.1
 mkdir build
@@ -102,15 +109,18 @@ cmake .. -G"MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF^
  -DCMAKE_TOOLCHAIN_FILE=%NDK%\build\cmake\android.toolchain.cmake -DANDROID_PLATFORM=%platformstr% -DANDROID_ABI=%%A^
  %CMAKE_DEFINITIONS%
 cmake --build .
-if errorlevel 1 exit /B
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 xcopy ..\include ..\..\include /s /e /y
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel%
 set FREETYPEBIN=libfreetype.a
 if /I "%~1" == "Debug" set FREETYPEBIN=libfreetyped.a
-copy %FREETYPEBIN% ..\..\lib\%%A\libfreetype.a
+copy !FREETYPEBIN! ..\..\lib\%%A\libfreetype.a
+if "!errorlevel!" neq "0" endlocal & exit /b %errorlevel% 
 cd ..\..
 rmdir /Q /S freetype-2.9.1
 del freetype-2.9.1.tar
-
 )
 
-@echo small3d dependencies built successfully for Android (%BUILDTYPE% mode)
+@echo small3d dependencies built successfully for Android ("%~1" mode)
+
+endlocal
