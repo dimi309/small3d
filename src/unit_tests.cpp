ab5167c18fedbc6f27fc719e297ed31ae350112b
dimi309 <dimi309@users.noreply.github.com>
2021-02-09 19:23:40 +0100

    Getting skins from glb file.

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -254,7 +254,7 @@ int GlbTest() {
 
   GlbFile glb("resources/models/goat.glb");
 
-  glb.printTokensRecursive();
+ /* glb.printTokensRecursive();
 
   glb.printTokensSerial();
 
@@ -270,11 +270,15 @@ int GlbTest() {
     printf("%f ", data[idx]);
   }
   printf("\n\r");
-  delete[] data;
+  delete[] data;*/
 
   GlbFile::Node n = glb.getNode("Bone");
 
   printf("Retrieved node from glb file: %s\n\r", n.name.c_str());
+
+  GlbFile::Skin s = glb.getSkin("Armature.001");
+
+  printf("Retrieved skin from glb file: %s\n\r", s.name.c_str());
 
   return 1;
 }
@@ -282,7 +286,7 @@ int GlbTest() {
 int main(int argc, char** argv) {
   try
   {
-    if (!LoggerTest()) {
+    /*if (!LoggerTest()) {
       printf("*** Failing LoggerTest.\n\r");
       return 1;
     }
@@ -317,7 +321,7 @@ int main(int argc, char** argv) {
     if (!TokenTest()) {
       printf("*** Failing TokenTest.\n\r");
       return 1;
-    }
+    }*/
     if (!GlbTest()) {
       printf("*** Failing GlbTest.\n\r");
       return 1;
