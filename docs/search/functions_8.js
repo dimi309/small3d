538e282b87c2edf705f34cb783b25937a43643c9
dimi309 <dimi309@users.noreply.github.com>
2021-04-05 07:45:54 +0200

    Documentation update

--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['model_220',['Model',['../classsmall3d_1_1_model.html#aa116b5b0c66b9e7f4dfd401ab5233a79',1,'small3d::Model::Model()'],['../classsmall3d_1_1_model.html#a0541e92c2861d9d5738f38909efba394',1,'small3d::Model::Model(File *file, const std::string &amp;meshName)']]]
+  ['model_220',['Model',['../classsmall3d_1_1_model.html#aa116b5b0c66b9e7f4dfd401ab5233a79',1,'small3d::Model::Model()'],['../classsmall3d_1_1_model.html#a5357ebf95632eb534178a4cb7f2c44a3',1,'small3d::Model::Model(File &amp;file, const std::string &amp;meshName)']]]
 ];
