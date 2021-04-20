5c0ab54af16640e42bf5075576e4cbc2531cbb2d
dimi309 <dimi309@users.noreply.github.com>
2021-04-20 07:49:10 +0200

    Adapted documentation generation.

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -7,5 +7,5 @@ var searchData=
   ['contains_10',['contains',['../classsmall3d_1_1_bounding_box_set.html#a4b5b507e6dae95b55943ec09d1d2d3bb',1,'small3d::BoundingBoxSet::contains()'],['../classsmall3d_1_1_scene_object.html#ae545d15364b5ecb0f4bc0d5cb5629ba4',1,'small3d::SceneObject::contains()']]],
   ['containscorners_11',['containsCorners',['../classsmall3d_1_1_bounding_box_set.html#a3f168bc8505d561601e94d9b77c4b07c',1,'small3d::BoundingBoxSet::containsCorners()'],['../classsmall3d_1_1_scene_object.html#a0cdbb5e24f06458dc5451acec3e373b7',1,'small3d::SceneObject::containsCorners()']]],
   ['createrectangle_12',['createRectangle',['../classsmall3d_1_1_renderer.html#acaa5ae15a3a21aaf43bbce93b5d3cdd7',1,'small3d::Renderer']]],
-  ['changelog_13',['changelog',['../md_small3d_changelog.html',1,'(Global Namespace)'],['../changelog.html',1,'(Global Namespace)']]]
+  ['changelog_13',['changelog',['../md_changelog.html',1,'(Global Namespace)'],['../changelog.html',1,'(Global Namespace)']]]
 ];
