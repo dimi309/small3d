43b7cc48447cb241c9dc688c160ca396aec0c4a1
dimi309 <dimi309@users.noreply.github.com>
2023-07-20 06:48:53 +0200

    Compressing native model files with zlib.

--- src/BinaryFile.cpp
+++ src/BinaryFile.cpp
@@ -15,6 +15,7 @@
 #include <cereal/types/memory.hpp>
 
 #include "BinaryFile.hpp"
+#include "zlib.h"
 
 #ifdef __ANDROID__
 #include "small3d_android.h"
@@ -59,8 +60,55 @@ void BinaryFile::load(Model& model, const std::string& meshName) {
   }
 #endif
 
-  cereal::BinaryInputArchive iarchive(is);
+  std::string readData = "";
+  const uint32_t CHUNK = 16384;
+
+  char rd[CHUNK];
+  while (!is.eof()) {
+    memset(rd, 0, CHUNK);
+    is.read(rd, CHUNK);
+    for (uint32_t idx = 0; idx < is.gcount(); ++idx) {
+      readData += rd[idx];
+    }
+  }
+  is.close();
+
+  unsigned char out[CHUNK];
+  uint32_t have = 0;
+  z_stream strm;
+  int flush;
+  strm.zalloc = Z_NULL;
+  strm.zfree = Z_NULL;
+  strm.opaque = Z_NULL;
+
+  if (inflateInit(&strm) != Z_OK) {
+    throw std::runtime_error("Failed to initialise inflate stream.");
+  }
+
+  strm.avail_in = readData.length();
+  strm.next_in = reinterpret_cast<Bytef*>(const_cast<char*>(readData.c_str()));
+  std::string uncompressedData = "";
+
+  do {
+    strm.avail_out = CHUNK;
+    strm.next_out = out;
+    if (inflate(&strm, Z_NO_FLUSH) == Z_STREAM_ERROR) {
+      LOGERROR("Stream error");
+    }
+    have = CHUNK - strm.avail_out;
+    for (uint32_t idx = 0; idx < have; ++idx) {
+      uncompressedData += out[idx];
+    }
+  } while (strm.avail_out == 0);
+  deflateEnd(&strm);
+  
+  std::istringstream iss(uncompressedData, std::ios::binary);
+
+  cereal::BinaryInputArchive iarchive(iss);
   iarchive(model);
+  iss.clear();
+  uncompressedData.clear();
+
 #ifdef __ANDROID__
   AAsset_close(asset);
 #else
--- src/ModelConverter/model_converter.cpp
+++ src/ModelConverter/model_converter.cpp
@@ -8,12 +8,18 @@
 #include <cereal/types/string.hpp>
 #include <cereal/types/memory.hpp>
 #include "BinaryFile.hpp"
+#include "zlib.h"
 using namespace small3d;
 
+const uint32_t CHUNK = 16384;
+
+
 int main(int argc, char** argv) {
+
   if (argc > 2) {
-    std::string modelpath(argv[1]);
-    std::string binpath(argv[2]);
+  
+    std::string modelpath = (argv[1]);
+    std::string binpath = (argv[2]);
 
     Model model;
 
@@ -31,17 +37,49 @@ int main(int argc, char** argv) {
       }
     }
 
-    std::ofstream os;
-
-    os.open(binpath, std::ios::out | std::ios::binary);
-
-    if (!os.is_open()) {
-      throw std::runtime_error("Could not open file " + binpath);
-    }
-    cereal::BinaryOutputArchive oarchive(os);
+    std::stringstream ss("", std::ios::out | std::ios::binary);
+    
+    cereal::BinaryOutputArchive oarchive(ss);
     oarchive(model);
-    os.flush();
-    os.close();
+    
+    unsigned char out[CHUNK];
+    uint32_t have = 0;
+    z_stream strm;
+    int flush;
+
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+
+    if (deflateInit(&strm, Z_DEFAULT_COMPRESSION) != Z_OK) {
+      std::cout << "Failed to initialise deflate stream." << std::endl;;
+      return 1;
+    }
+    
+    auto strBuffer = ss.str();
+
+    strm.next_in = reinterpret_cast<Bytef*>(const_cast<char*>(strBuffer.c_str()));
+    strm.avail_in = strBuffer.length();
+    std::string compressedData = "";
+    int defRet = 0;
+    do {
+      strm.avail_out = CHUNK;
+      strm.next_out = out;
+     
+      defRet = deflate(&strm, strm.avail_in > 0 ? Z_NO_FLUSH : Z_FINISH);
+      if (defRet == Z_STREAM_ERROR) {
+        LOGERROR("Stream error");
+      }
+      have = CHUNK - strm.avail_out;
+      for (uint32_t idx = 0; idx < have; ++idx) {
+        compressedData += out[idx];
+      }
+    } while (defRet != Z_STREAM_END);
+    deflateEnd(&strm);
+
+    std::ofstream ofstr(binpath, std::ios::out | std::ios::binary);
+    ofstr.write(compressedData.c_str(), compressedData.length());
+    ofstr.close();
 
     try {
       Model m1(BinaryFile(binpath), "");
