4815b99019ed4475365dd89eb1700b63c31666b7
dimi309 <dimi309@users.noreply.github.com>
2024-10-27 06:24:38 +0100

    Using stdlib exit codes.

--- src/UnitTests/unit_tests.cpp
+++ src/UnitTests/unit_tests.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
   { 
     if (!LoggerTest()) {
       printf("*** Failing LoggerTest.\n\r");
-      return 1;
+      return EXIT_FAILURE;
     }
 
     small3d::initLogger();
@@ -26,116 +26,116 @@ int main(int argc, char** argv) {
 #ifdef _WIN32
     if (!ScreenCaptureTest()) {
       LOGINFO("*** Failing ScreenCaptureTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("ScreenCaptureTest OK");
 
     if (!ControllerTest()) {
       LOGINFO("*** Failing ControllerTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("ControllerTest OK");
 #endif
 
     if (!ImageTest()) {
       LOGINFO("*** Failing ImageTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("ImageTest OK");
 
     if (!WavefrontFailTest()) {
       LOGINFO("*** Failing WavefrontFailTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("WavefrontFailTest OK");
     
     if (!WavefrontModelTest()) {
       LOGINFO("*** Failing WavefrontModelTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("WavefrontModelTest OK");
 
     if (!ScaleAndTransformTest()) {
       LOGINFO("*** Failing ScaleAndTransformTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("ScaleAndTransformTest OK");
     
     if (!GlbTextureTestDefaultShadows()) {
       LOGINFO("*** Failing GlbTextureTestDefaultShadows.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("GlbTextureTestDefaultShadows OK");
     
     if (!GlbTextureTestLookAtShadows()) {
       LOGINFO("*** Failing GlbTextureTestLookAtShadows.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("GlbTextureTestLookAtShadows OK");
     
     if (!BoundingBoxesTest()) {
       LOGINFO("*** Failing BoundingBoxesTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("BoundingBoxesTest OK");
     
     if (!FPStest()) {
       LOGINFO("*** Failing FPStest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("FPStest OK");
 
     if (!GenericSceneObjectConstructorTest()) {
       LOGINFO("*** Failing GenericSceneObjectConstructorTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("GenericSceneObjectConstructorTest OK");
     
     if (!RendererTest()) {
       LOGINFO("*** Failing RendererTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("RendererTest OK");
 
     if (!BinaryModelTest()) {
       LOGINFO("*** Failing BinaryModelTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("BinaryModelTest OK");
 
     if (!SoundTest()) {
       LOGINFO("*** Failing SoundTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("SoundTest OK");
 
     if (!BinSoundTest()) {
       LOGINFO("*** Failing BinSoundTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("BinSoundTest OK");
 
     if (!SoundTest2()) {
       LOGINFO("*** Failing SoundTest2.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("SoundTest2 OK");
 
     if (!SoundTest3()) {
       LOGINFO("*** Failing SoundTest3.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("SoundTest3 OK");
     
     if (!GlbTest()) {
       LOGINFO("*** Failing GlbTest.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("GlbTest OK");
 
     if (!ModelsTimeToLoad()) {
       LOGINFO("*** Failing ModelsTimeToLoad.");
-      return 1;
+      return EXIT_FAILURE;
     }
     LOGINFO("ModelsTimeToLoad OK");
 
@@ -145,8 +145,8 @@ int main(int argc, char** argv) {
     // Also use printf in case the logger has not been initialised properly
     printf("*** Exception thrown during unit tests: %s\n\r", e.what());
     LOGERROR("*** Exception thrown during unit tests: " + std::string(e.what()));
-    return 1;
+    return EXIT_FAILURE;
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
