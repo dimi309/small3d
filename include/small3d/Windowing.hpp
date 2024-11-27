a4af1110fc301b10db4a862d1e01d2417affe185
dimi309 <dimi309@users.noreply.github.com>
2024-11-27 05:49:38 +0100

    Added windowing environment management class.

---
+++ include/small3d/Windowing.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file Windowing.hpp
+ * @brief Windowing environment management class
+ *
+ * Created on: 2024/11/27
+ *     Author: Dimitri Kourkoulis
+ *    License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#pragma once
+
+namespace small3d {
+  class Windowing {
+  }
+}
---
+++ src/Windowing.cpp
@@ -0,0 +1,9 @@
+/*
+ *  Windowing.cpp
+ *
+ *  Created on: 2024/11/27
+ *      Author: Dimitri Kourkoulis
+ *     License: BSD 3-Clause License (see LICENSE file)
+ */
+
+#include "Windowing.hpp"
