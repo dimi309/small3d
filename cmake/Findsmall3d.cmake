79ef3bef4492a424c0afea13ed8a944b1544922e
dimi309 <dimi309@users.noreply.github.com>
2024-12-07 20:14:41 +0100

    Removed bzip2 requirement from mingw build. It seems it is no longer needed.

--- cmake/Findsmall3d.cmake
+++ cmake/Findsmall3d.cmake
@@ -62,8 +62,7 @@ if(APPLE)
               -framework IOKit -framework CoreVideo")
 endif()
 
-if(UNIX OR (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" AND NOT
-      CMAKE_CXX_COMPILER_VERSION VERSION_LESS 10))
+if(UNIX)
   set(small3d_INCLUDE_DIRS ${small3d_INCLUDE_DIRS} ${BZIP2_INCLUDE_DIRS})
   set(small3d_LIBRARIES ${small3d_LIBRARIES} ${BZIP2_LIBRARIES})
 endif()
